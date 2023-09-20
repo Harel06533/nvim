71d00110a0842b976bd522c6222c737fecfa9735
Harel065 <harelolguin22@gmail.com>
2023-09-20 13:23:48 -0600

    silent tree leader

--- lua/harel065/keymaps.lua
+++ lua/harel065/keymaps.lua
@@ -5,7 +5,7 @@ vim.g.mapleader = " "
 keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 --nvim-tree
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 --store
 keymap.set("n", "<leader>w", vim.cmd.write)
