6373eab03902c8c960b48b7604e2b0a463907f1e
Harel065 <harelolguin22@gmail.com>
2023-09-27 13:25:19 -0600

    moved diagnostics to lspsaga

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -22,10 +22,6 @@ end
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local opts = { noremap = true, silent = true }
-
-  buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-
-  buf_set_keymap("n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 end
 
 protocol.CompletionItemKind = {
