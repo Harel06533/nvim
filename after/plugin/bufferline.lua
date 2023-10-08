4fe4f32f95536dfdfeefd844c2178af3606552d4
Harel065 <harelolguin22@gmail.com>
2023-10-08 00:15:39 -0600

    customized bufferline colors

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -4,7 +4,35 @@ if not status then
   return
 end
 
-buffline.setup()
+buffline.setup {
+  options = {
+    mode = "buffers",
+    separator_style = "slant",
+    always_show_bufferline = false,
+    show_buffer_close_icons = false,
+    color_icons = true
+  },
+  highlights = {
+    separator = {
+      fg = '#191c29',
+      bg = '#191c29'
+    },
+    separator_selected = {
+      fg = '#191c29'
+    },
+    background = {
+      fg = '#464a5d',
+      bg = '#191c29'
+    },
+    buffer_selected = {
+      fg = '#fdf6e3',
+      bold = true,
+    },
+    fill = {
+      bg = '#191c29'
+    }
+  },
+}
 
 vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", {})
 vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", {})
