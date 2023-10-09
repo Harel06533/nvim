b00de2bc1e9d0f0f29a8de2e8313087f2111a319
Harel065 <harelolguin22@gmail.com>
2023-10-09 13:38:11 -0600

    changed some lsp icons

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -32,7 +32,7 @@ protocol.CompletionItemKind = {
   '', -- Field
   '', -- Variable
   '', -- Class
-  'ﰮ', -- Interface
+  '◍', -- Interface
   '', -- Module
   '', -- Property
   '', -- Unit
@@ -52,7 +52,7 @@ protocol.CompletionItemKind = {
   '', -- TypeParameter
 }
 
-local signs = { Error = " ", Warn = " ", Hint = "! ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = "◉ ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
