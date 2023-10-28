be2806c24cb865b94673392b08b2ef146970071a
Harel065 <harelolguin22@gmail.com>
2023-10-28 14:35:34 -0600

    changed cmdheight to 0, deleted prettier plugin and added telescope keymap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,10 +6,15 @@ end
 vim.keymap.set("n", "<leader>th", builtin.find_files)
 vim.keymap.set("n", "<leader>gf", builtin.git_files)
 
-require("telescope").load_extension "file_browser"
+require("telescope").load_extension("file_browser")
+
+-- opens file browser in the current working directory
+vim.api.nvim_set_keymap("n", "<leader>tt", ":Telescope file_browser<CR>", { noremap = true })
+
+-- open file browser from the current buffer
 vim.api.nvim_set_keymap(
   "n",
-  "<leader>tt",
-  ":Telescope file_browser<CR>",
-  { noremap = true }
+  "<leader>tu",
+  ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+  { noremap = true, silent = true }
 )
